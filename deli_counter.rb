0d1daf988d73ea19b8cca71123690483674deaf5
Luis Leandro <luchrist777@gmail.com>
2020-05-16 02:03:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(cust)
+  serving = []
+  if cust.size == 0
+    puts "the ling is currently empty"
+  elsif customers.size >= 1
+  cust. each_with_index do |cust, index|
+    serving <<
+end
+  
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
+
+def now_serving(array)
+  
+  
